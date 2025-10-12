cab487f9c6e15ed5f4bb4b21e393e979559acb91
Letícia Militão <leticia.militao.v.b@gmail.com>
2025-10-12 15:35:43 -0300

    Update aula1.py

--- aula1.py
+++ aula1.py
@@ -10,8 +10,21 @@ if nome:
 st.title("Lista de Exercícios 1")
 
 st.write("1. A partir de um número informado pelo usuário, indique se um número inteiro qualquer é par ou ímpar.")
-numero = int.text_input("Informe um número inteiro: "))
+numero = int.text_input("Informe um número inteiro: ")
 if numero % 2 == 0:
     st.text_input(f"O número {numero} é par.")
 else:
     st.text_input(f"O número {numero} é ímpar.")
+
+st.write("2. Desenvolva um programa que armazene quatro notas em uma lista e que apresente: a média final, a maior nota e a menor nota.")
+nota1 = float.text_input("Informe a primeira nota: ")
+nota2 = float.text_input("Informe a segunda nota: ")
+nota3 = float.text_input("Informe a terceira nota: ")
+nota4 = float.text_input("Informe a quarta nota: ")
+notas = [nota1, nota2, nota3, nota4]
+media = sum.notas / len.notas
+maior_nota = max(notas)
+menor_nota = min(notas)
+st.write(f"A média final é: {media}")
+st.write(f"A maior nota é: {maior_nota}")
+st.write(f"A menor nota é: {menor_nota}")
