5847d99e428c63c548d2eb65c73aee5a9a105d03
Letícia Militão <leticia.militao.v.b@gmail.com>
2025-10-12 15:28:47 -0300

    aula1.py

--- aula1.py
+++ aula1.py
@@ -10,8 +10,8 @@ if nome:
 st.title("Lista de Exercícios 1")
 
 st.write("1. A partir de um número informado pelo usuário, indique se um número inteiro qualquer é par ou ímpar.")
-numero = int(input("Informe um número inteiro: "))
+numero = int.text_input("Informe um número inteiro: "))
 if numero % 2 == 0:
-    print(f"O número {numero} é par.")
+    st.text_input(f"O número {numero} é par.")
 else:
-    print(f"O número {numero} é ímpar.")
+    st.text_input(f"O número {numero} é ímpar.")
