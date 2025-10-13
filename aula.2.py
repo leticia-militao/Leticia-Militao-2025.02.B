c78d9aaca5d61840a40448f8641d819aad6119a8
Letícia Militão <leticia.militao.v.b@gmail.com>
2025-10-13 15:11:13 -0300

    Update aula.2.py

--- aula.2.py
+++ aula.2.py
@@ -1,6 +1,15 @@
 import pandas as pd
 import streamlit as st
+import plotly.express as st
 
 dataset = pd.read_csv('https://www.irdx.com.br/media/uploads/paises.csv')
 
-st.dataframe(dataset)
+fig = px.choropleth(dataset,
+                    locations=dataset['iso3'],
+                    color=dataset['latitude'],
+                    hover_name=dataset['nome'])
+fig.update_layout(title='Mapa Coroplético dos Países',
+                  geo_scope='world')
+st.plotly_chart(fig,
+                use_container_widht=True,
+                theme='streamlit')
