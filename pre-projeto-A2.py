7c8a60f2cec11750a6b45459132cad041b5b20a2
Letícia Militão <leticia.militao.v.b@gmail.com>
2025-10-20 14:58:57 -0300

    pre-projeto-A2.py

---
+++ pre-projeto-A2.py
@@ -0,0 +1,8 @@
+import requests
+import json
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+st.title("Câmara Aberta")
+st.write("Uma plataforma dedicada ao acesso a informações da Câmara dos Deputados")
