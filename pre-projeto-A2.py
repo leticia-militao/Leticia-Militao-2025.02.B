d0509fe04c23e8cce327fbff25713cf99438ec9b
Letícia Militão <leticia.militao.v.b@gmail.com>
2025-10-27 14:37:20 -0300

    Update pre-projeto-A2.py

--- pre-projeto-A2.py
+++ pre-projeto-A2.py
@@ -5,7 +5,7 @@ import streamlit as st
 import plotly.express as px
 
 st.title("Câmara Aberta")
-st.subtitle("Uma plataforma dedicada ao acesso a informações da Câmara dos Deputados")
+st.header("Uma plataforma dedicada ao acesso a informações da Câmara dos Deputados")
 st.write("Que tipo de busca você deseja fazer?")
 st. write("1 - Projetos de Lei")
 st. write("2 - Propostas de Emenda Constitucional")
