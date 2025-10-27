775387bee7b6823338563535d9e3e616d22bcab0
Letícia Militão <leticia.militao.v.b@gmail.com>
2025-10-27 14:34:28 -0300

    Update pre-projeto-A2.py

--- pre-projeto-A2.py
+++ pre-projeto-A2.py
@@ -6,9 +6,9 @@ import plotly.express as px
 
 st.title("Câmara Aberta")
 st.subtitle("Uma plataforma dedicada ao acesso a informações da Câmara dos Deputados")
-st.write("Que tipo de busca você deseja fazer?
-         1 - Projetos de Lei
-         2 - Propostas de Emenda Constitucional
-         3 - Deputados
-         4 - Sair")
+st.write("Que tipo de busca você deseja fazer?")
+st. write("1 - Projetos de Lei")
+st. write("2 - Propostas de Emenda Constitucional")
+st. write("3 - Deputados")
+st. write("4 - Sair")
 busca = st.text_input("Digite o número da opção da sua busca:")
