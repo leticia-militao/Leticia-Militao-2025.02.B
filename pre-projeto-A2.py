3ccf562be6f63c9b589d94394ccc02e7d04dfebe
Letícia Militão <leticia.militao.v.b@gmail.com>
2025-10-27 14:43:58 -0300

    Update pre-projeto-A2.py

--- pre-projeto-A2.py
+++ pre-projeto-A2.py
@@ -8,6 +8,5 @@ st.title("Câmara Aberta")
 st.header("Uma plataforma dedicada ao acesso a informações da Câmara dos Deputados")
 st.write("Que tipo de busca você deseja fazer?")
 opcoes = ["1 - Projetos de Lei", "2 - Propostas de Emenda Constitucional", "3 - Deputados", "4 -  Sair"]
-st. write(opcoes)
 busca = st.radio("Digite o número da opção da sua busca:", opcoes)
-at.write("Você escolheu:{busca}")
+st.write("Você escolheu:{busca}")
