fb847462e6bb0c1f28c3e70aeb55056061e4fd0c
Letícia Militão <leticia.militao.v.b@gmail.com>
2025-10-27 15:01:10 -0300

    Update pre-projeto-A2.py

--- pre-projeto-A2.py
+++ pre-projeto-A2.py
@@ -7,7 +7,7 @@ import plotly.express as px
 st.title("Câmara Aberta")
 st.header("Uma plataforma dedicada ao acesso a informações da Câmara dos Deputados")
 st.write("Que tipo de busca você deseja fazer?")
-opcoes = ["1 - Projetos de Lei", "2 - Propostas de Emenda Constitucional", "3 - Deputados", "4 -  Sair"]
+opcoes = ["1 - Projetos de Lei", "2 - Propostas de Emenda Constitucional", "3 - Deputados", "4 - Sair"]
 busca = st.radio("Digite o número da opção da sua busca:", opcoes)
 if busca == "1 - Projetos de Lei":
   st.write("Você escolheu a opção de buscar projetos de lei.")
