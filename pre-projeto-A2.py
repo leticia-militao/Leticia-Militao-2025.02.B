c53e812ba9d1c7579c4089392ec4043a39a584c2
Letícia Militão <leticia.militao.v.b@gmail.com>
2025-10-27 15:00:32 -0300

    Update pre-projeto-A2.py

--- pre-projeto-A2.py
+++ pre-projeto-A2.py
@@ -9,4 +9,12 @@ st.header("Uma plataforma dedicada ao acesso a informações da Câmara dos Depu
 st.write("Que tipo de busca você deseja fazer?")
 opcoes = ["1 - Projetos de Lei", "2 - Propostas de Emenda Constitucional", "3 - Deputados", "4 -  Sair"]
 busca = st.radio("Digite o número da opção da sua busca:", opcoes)
-st.write("Você escolheu: {busca}")
+if busca == "1 - Projetos de Lei":
+  st.write("Você escolheu a opção de buscar projetos de lei.")
+if busca == "2 - Propostas de Emenda Constitucional":
+  st.write("Você escolheu a opção de buscar propostas de emenda constitucional.")
+if busca == "3 - Deputados":
+  st.write("Você escolheu a opção de buscar informações de deputados.")
+if busca == "4 - Sair":
+  st.write("Você escolheu a opção de sair da pesquisa.")
+  st.write("Obrigado por usar o programa. Até a próxima!")
